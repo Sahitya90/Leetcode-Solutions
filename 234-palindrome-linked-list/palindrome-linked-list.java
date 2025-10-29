48f2b9eb16edc9461d75cee8f608633762f02c8c
Sahitya90 <sahityavikramsingh@gmail.com>
2025-10-29 17:00:06 +0530

    Time: 3 ms (99.75%) | Memory: 68.8 MB (28.06%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.java
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -8,31 +8,79 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
+
+ // this is the solution for this question using brute force approach
 class Solution {
     public boolean isPalindrome(ListNode head) {
         
-        List <Integer> list = new ArrayList<>();
-        ListNode current = head;
+//         List <Integer> list = new ArrayList<>();
+//         ListNode current = head;
 
-        while(current != null){
-            list.add(current.val);
-            current = current.next;
-        }
+//         while(current != null){
+//             list.add(current.val);
+//             current = current.next;
+//         }
 
-        int start = 0;
-        int end = list.size() - 1;
+//         int start = 0;
+//         int end = list.size() - 1;
 
-        while(start<end){
-            if(!list.get(start).equals(list.get(end))){
-                return false;
-            }
+//         while(start<end){
+//             if(!list.get(start).equals(list.get(end))){
+//                 return false;
+//             }
             
                 
-                start++;
-                end--;
+//                 start++;
+//                 end--;
             
-        }
-        return true;
+//         }
+//         return true;
         
+//     }
+// }
+
+//now we will be discussing the optimal approach for this question: 
+    ListNode slow = head;
+    ListNode fast = head;
+    
+    
+
+    while(fast != null && fast.next != null){
+
+        slow = slow.next;
+        fast = fast.next.next;
+
+        
+
+    }
+    
+
+    ListNode prev = null;
+    ListNode curr = slow;
+    ListNode nextTemp = null;
+
+    while(curr != null){
+        nextTemp = curr.next;
+        curr.next = prev;
+        prev = curr;
+        curr = nextTemp;
+
+       
+    }
+    
+    ListNode left = head;
+    ListNode right = prev;
+
+    while(right != null){
+        if(left.val != right.val){
+            return false;
+        }
+        left = left.next;
+        right = right.next;
+    }
+    return true;
+
+
     }
 }
