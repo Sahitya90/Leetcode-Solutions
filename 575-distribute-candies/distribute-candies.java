e957dc1334fd54166e7f32017a3041a8e2e6660a
Sahitya90 <sahityavikramsingh@gmail.com>
2026-03-13 11:15:29 +0530

    Time: 31 ms (57.87%) | Memory: 48.6 MB (48.74%) - LeetSync

--- 575-distribute-candies/distribute-candies.java
+++ 575-distribute-candies/distribute-candies.java
@@ -1,16 +1,14 @@
 class Solution {
     public int distributeCandies(int[] candyType) {
-        
         int n = candyType.length;
-
         int maximumCandy = n/2;
 
-        Set<Integer> uniqueCandies = new HashSet<>();
+        Set<Integer>uniqueCandies = new HashSet<>();
 
         for(int candy : candyType){
-
             uniqueCandies.add(candy);
         }
         return Math.min(maximumCandy, uniqueCandies.size());
+
     }
 }
