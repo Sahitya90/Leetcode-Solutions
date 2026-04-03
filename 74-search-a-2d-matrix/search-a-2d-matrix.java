9a43874e87de8c6bffe5e19fa2e7c0d744f44087
Sahitya Vikram Singh <sahityavikramsingh@gmail.com>
2026-04-03 14:16:26 +0530

    Time: 0 ms (100.00%) | Memory: 43.9 MB (52.13%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -1,19 +1,22 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        int row = matrix.length;
-        int col = matrix[0].length;
+        
+        int row = matrix.length -1;
+        int col = matrix[0].length -1;
 
+        int left = 0;
+        int right = col;
         int top = 0;
-        int bottom = row - 1 ;
-        int targetRow=0;
+        int bottom = row;
+        int targetRow = 0;
 
         while(top<=bottom){
-            int mid = top + (bottom - top)/2;
+            int mid = top + (bottom - top) /2;
 
-            if(target > matrix[mid][col-1]){
-                top = mid + 1;
+            if(target > matrix[mid][right]){
+                top = mid +1;
             }
-            else if(target < matrix[mid][0]){
+            else if(target < matrix[mid][left]){
                 bottom = mid -1;
             }
             else{
@@ -21,24 +24,21 @@ class Solution {
                 break;
             }
         }
-        
-        int left = 0; 
-        int right = col -1;
+
         while(left<=right){
-           int mid = left + (right - left)/2;
-            
-            if(target > matrix[targetRow][mid]){
+            int mid = left + (right - left) /2;
+
+            if(target> matrix[targetRow][mid]){
                 left = mid + 1;
             }
             else if(target < matrix[targetRow][mid]){
-                right = mid - 1;
+                right = mid -1;
             }
             else{
                 return true;
             }
-
         }
-        return false;
+       return false;
 
     }
 }
