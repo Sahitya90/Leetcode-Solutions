d48fc2732f3ede1ecf77f478b3cbf58c0a9cfd20
Sahitya Vikram Singh <sahityavikramsingh@gmail.com>
2026-05-20 17:33:46 +0530

    Time: 4 ms (93.35%) | Memory: 47.4 MB (19.79%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,5 +1,6 @@
 class Solution {
-
+    
+    //this dfs function here is to check whether there is a cycle or not
     public boolean dfs(int curr, boolean vis[], boolean rec[], List<List<Integer>> graph){
 
         vis[curr] = true;
@@ -8,25 +9,36 @@ class Solution {
         for(int neighbour : graph.get(curr)){
 
             if(rec[neighbour]){
+                //if already in the rec array, then cycle found
                 return true;
             }else if(!vis[neighbour]){
+                //if not visited, then recursive call
                 if(dfs(neighbour, vis, rec, graph)){
+                    //if found cycle wihtin then, return true
                     return true;
                 }
             }
         }
+        //if no cycle found for all the neighbours starting from curr, then remove curr from the rec array to further prevent the confusion for the function
         rec[curr] = false;
+        //return false as cycle found till last
         return false;
 
     }
+    //this function tells whether we can finish the course or not
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        
+
+
+        //creating graph using arraylist or arraylist
         List<List<Integer>> graph = new ArrayList<>();
+
+        //traversing to intialize how many number of elements will be there in the graph list
         for(int i = 0; i<numCourses; i++){
             graph.add(new ArrayList<>());
         }
 
-
+        
+        //add, the neighbour for a particular node into its assigned arraylist
         for(int[] pre : prerequisites){
             int course = pre[0];
             int requirement = pre[1];
@@ -37,6 +49,8 @@ class Solution {
         boolean vis[] = new boolean[numCourses];
         boolean rec[] = new boolean[numCourses];
 
+        
+        //decide how many times does the dfs function goes on for
         for(int i = 0; i<numCourses; i++){
             if(vis[i]==false){
                 if(dfs(i, vis, rec, graph)){
