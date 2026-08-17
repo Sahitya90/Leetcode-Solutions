713ab4e9d3f30771f84228279604d8e3504afe4a
Sahitya Vikram Singh <sahityavikramsingh@gmail.com>
2026-08-17 12:07:03 +0530

    Time: 2 ms (91.98%) | Memory: 47.7 MB (76.71%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.java
@@ -0,0 +1,17 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        
+        int left = 0;
+
+        for(int right = 0; right < nums.length; right++){
+
+            if(nums[right] != 0){
+                int t = nums[right];
+                nums[right] = nums[left];
+                nums[left] = t;
+
+                left++;
+            }
+        }
+    }
+}
