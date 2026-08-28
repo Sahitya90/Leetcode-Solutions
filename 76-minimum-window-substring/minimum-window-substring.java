e6a4908b4ddbe01c3809fcb511d1d6f5072cb58d
Sahitya Vikram Singh <sahityavikramsingh@gmail.com>
2026-08-28 15:38:05 +0530

    Time: 3 ms (72.61%) | Memory: 44.8 MB (99.79%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -3,54 +3,45 @@ class Solution {
         
         int m = s.length();
         int n = t.length();
-
-        int[] a1 = new int[128];
-        int[] a2 = new int[128];
         int have = 0;
         int need = 0;
-        int minWin = Integer.MAX_VALUE;
+        int minLen = Integer.MAX_VALUE;
         int startIndex = -1;
+        int left = 0;
+        int[] a1 = new int[128];
+        int[] a2 = new int[128];
 
-        for(char c: t.toCharArray()){
-            if(a1[c] == 0){
+        for(int i = 0; i<n; i++){
+            char a = t.charAt(i);
+            if(a1[a] == 0){
                 need++;
             }
-            a1[c]++;
+            a1[a]++;
+
         }
-        int left = 0;
-        
         for(int right = 0; right<m; right++){
 
             char val = s.charAt(right);
+            
             a2[val]++;
 
-            if( a1[val] > 0 && a1[val] == a2[val]){
+
+            if(a1[val] > 0 && a1[val] == a2[val]){
                 have++;
             }
+            while(need == have){
 
-            while(have == need){
-                //store the value of minWin, in the case it is less than current minWin, also store the startPostion on left 
-
-                if(right - left + 1 < minWin){
-                    minWin = right -left + 1;
+                if(right - left + 1 < minLen){
+                    minLen = right - left + 1;
                     startIndex = left;
-
                 }
-                //shrink the left element
-                char leftChar = s.charAt(left);
-                a2[leftChar]--;
-
-                //on shrinking if the frequency of both arrays get unequal we reduce the value have              
-                if(a1[leftChar] > 0 && a2[leftChar] < a1[leftChar]){
+                a2[s.charAt(left)]--;
+                if(a1[s.charAt(left)] > 0 && a2[s.charAt(left)] < a1[s.charAt(left)]){
                     have--;
                 }
-
-                //move the left pointer forward
                 left++;
             }
-                
         }
-        return minWin == Integer.MAX_VALUE ? "" : s.substring(startIndex, startIndex + minWin);
-
+        return minLen == Integer.MAX_VALUE ? "" : s.substring(startIndex, startIndex + minLen);
     }
 }
