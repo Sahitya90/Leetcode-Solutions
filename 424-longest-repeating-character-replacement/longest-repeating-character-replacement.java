99248bc5b9f910dce00d2684d5241f2a2d12e784
Sahitya Vikram Singh <sahityavikramsingh@gmail.com>
2026-08-28 16:08:00 +0530

    Time: 6 ms (98.89%) | Memory: 46.3 MB (50.69%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -4,7 +4,7 @@ class Solution {
         int left = 0;
         int n = s.length();
         int maxFreq = 0;
-        int[] freq = new int[126];
+        int[] freq = new int[26];
         int win = Integer.MIN_VALUE;
 
         for(int right = 0; right< n; right++){
