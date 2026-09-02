dc642bb2a83be5746d7476853543a5e1f78643b1
Sahitya Vikram Singh <sahityavikramsingh@gmail.com>
2026-09-02 13:01:19 +0530

    Time: 1 ms (81.47%) | Memory: 43.5 MB (90.72%) - LeetSync

--- 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.java
@@ -13,45 +13,43 @@
  *     }
  * }
  */
-import java.util.*; 
+import java.util.Collections;
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+
+        if(root == null){
+            return new ArrayList<>();
+        }
+
         List<List<Integer>> result = new ArrayList<>();
 
-        if(root==null){
-            return result;
-        }
+        Queue<TreeNode> q = new LinkedList<>();
+        boolean reverse = false;
+        q.add(root);
 
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.add(root);
-        boolean flag = true; 
- 
-        while(!queue.isEmpty()){
-            int size = queue.size();
-            LinkedList<Integer> LL = new LinkedList<>();
+        while(!q.isEmpty()){
 
+            int size = q.size();
+            List<Integer> list = new ArrayList<>();
             for(int i = 0; i<size; i++){
-                TreeNode temp = queue.poll();
+                TreeNode temp = q.poll();
+                list.add(temp.val);
 
-                if(flag){
-                    LL.addLast(temp.val);
+                if(temp.left != null){
+                    q.add(temp.left);
                 }
-                else{
-                    LL.addFirst(temp.val);
+                if(temp.right != null){
+                    q.add(temp.right);
                 }
-
-                if(temp.left!=null){
-                    queue.add(temp.left);
-                }
-                if(temp.right!=null){
-                    queue.add(temp.right);
-                }
-
-
             }
-            result.add(LL);
-            flag= !flag;
+
+            if(reverse == true){
+                Collections.reverse(list);
+            }
+            result.add(list); 
+            reverse =  !reverse;           
         }
         return result;
+        
     }
 }
