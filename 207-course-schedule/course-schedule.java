d288e226040c86866ed3547a5c36a76dafea015d
Sahitya Vikram Singh <sahityavikramsingh@gmail.com>
2026-09-03 15:59:49 +0530

    Time: 30 ms (5.23%) | Memory: 46.7 MB (95.96%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,59 +1,37 @@
 class Solution {
-    
-    //this dfs function here is to check whether there is a cycle or not
-    public boolean dfs(int curr, boolean vis[], boolean rec[], List<List<Integer>> graph){
+    public boolean isCycleDFS(int src, int[][] pre, boolean[] vis, boolean[] rec){
 
-        vis[curr] = true;
-        rec[curr] = true;
+        vis[src] = true;
+        rec[src] = true;
 
-        for(int neighbour : graph.get(curr)){
+        for(int i =0 ;i<pre.length; i++){
 
-            if(rec[neighbour]){
-                //if already in the rec array, then cycle found
-                return true;
-            }else if(!vis[neighbour]){
-                //if not visited, then recursive call
-                if(dfs(neighbour, vis, rec, graph)){
-                    //if found cycle wihtin then, return true
+            int u = pre[i][0];
+            int v = pre[i][1];
+
+            if(u == src){
+                if(!vis[v]){
+
+                    if(isCycleDFS(v, pre, vis, rec)){
+                        return true;
+                    }
+                }else if(rec[v]){
                     return true;
                 }
-            }
+            }    
         }
-        //if no cycle found for all the neighbours starting from curr, then remove curr from the rec array to further prevent the confusion for the function
-        rec[curr] = false;
-        //return false as cycle found till last
-        return false;
-
+        rec[src] = false;
+        return false; 
     }
-    //this function tells whether we can finish the course or not
-    public boolean canFinish(int numCourses, int[][] prerequisites) {
-
-
-        //creating graph using arraylist or arraylist
-        List<List<Integer>> graph = new ArrayList<>();
-
-        //traversing to intialize how many number of elements will be there in the graph list
-        for(int i = 0; i<numCourses; i++){
-            graph.add(new ArrayList<>());
-        }
-
+    public boolean canFinish(int n, int[][] pre) {
         
-        //add, the neighbour for a particular node into its assigned arraylist
-        for(int[] pre : prerequisites){
-            int course = pre[0];
-            int requirement = pre[1];
+        boolean[] vis = new boolean[n];
+        boolean[] rec = new boolean[n];
 
-            graph.get(requirement).add(course);
-        }
-
-        boolean vis[] = new boolean[numCourses];
-        boolean rec[] = new boolean[numCourses];
-
-        
-        //decide how many times does the dfs function goes on for
-        for(int i = 0; i<numCourses; i++){
-            if(vis[i]==false){
-                if(dfs(i, vis, rec, graph)){
+        for(int i = 0; i<n; i++){
+            
+            if(!vis[i]){
+                if(isCycleDFS(i, pre, vis, rec)){
                     return false;
                 }
             }
